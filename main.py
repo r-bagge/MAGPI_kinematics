aa5ce25c8b0e92d34b2ef5571ec226b813e6aee0
r-bagge <r.bagge@unsw.edu.au>
2023-08-01 15:46:40 +1000

    Hopefully fixed up sample selection

--- main.py
+++ main.py
@@ -530,7 +530,7 @@ if __name__ == '__main__':
                            "SNR_g": results[8],
                            "SNR_s": results[9],
                            })
-        df.to_csv("MAGPI_csv/MAGPI_kinemetry_sample_S05.csv")
+        df.to_csv("MAGPI_csv/MAGPI_kinemetry_sample_s05.csv")
         print(f"Final sample is {len(df):.0f} out of {len(file):.2f}")
 
     else:
@@ -559,6 +559,6 @@ if __name__ == '__main__':
                            "SNR_g": results[8],
                            "SNR_s": results[9],
                            })
-        df.to_csv("MAGPI_csv/MAGPI_kinemetry_sample_S05.csv")
+        df.to_csv("MAGPI_csv/MAGPI_kinemetry_sample_s05_no_err.csv")
         print(f"Final sample is {len(df):.0f} out of {len(file):.2f}")
     BPT_plots("MAGPI_csv/MAGPI_kinemetry_sample_BPT.csv", "MAGPI_csv/MAGPI_kinemetry_sample.csv", n_re=1.5)
