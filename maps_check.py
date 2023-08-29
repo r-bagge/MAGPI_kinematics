d525be8e08d6bed49eba4c549ee2ba80e3821bac
r-bagge <r.bagge@unsw.edu.au>
2023-08-29 16:47:30 +1000

    NaNs are finally fixed!

--- maps_check.py
+++ maps_check.py
@@ -276,7 +276,7 @@ def maps_check():
 
 def vasyms_nans():
     sample = pd.read_csv("MAGPI_csv/MAGPI_kinemetry_sample_M2.csv")
-    bpt = pd.read_csv("MAGPI_csv/MAGPI_kinemetry_sample_M2_BPT.csv")
+    #bpt = pd.read_csv("MAGPI_csv/MAGPI_kinemetry_sample_M2_BPT.csv")
     try:
         logfile = open("/Users/ryanbagge/Library/CloudStorage/OneDrive-UNSW/MAGPI_Plots/log.txt", "w")
     except FileNotFoundError:
@@ -294,8 +294,8 @@ def vasyms_nans():
         r50 = master['R50_it'].to_numpy() / 0.2
 
         vasyms = sample[sample["MAGPIID"].isin([g])]
-        bpts = bpt[bpt["MAGPIID"].isin([g])]
-        ty = bpt["type (sf+AGN=0, sf=1, sy=2, ln=3)"].to_numpy()
+        # bpts = bpt[bpt["MAGPIID"].isin([g])]
+        # ty = bpt["type(sf+AGN=0, sf=1, sy=2, ln=3)"].to_numpy()
         try:
             gasfile = fits.open(
                 "MAGPI_Maps/MAGPI" + field + "/Emission_Line/MAGPI" + str(
@@ -334,8 +334,8 @@ def vasyms_nans():
     vasym_err = np.array(vasym_err)
 
     fig,ax = plt.subplots()
-    ax.scatter((n_nans/n_not_nans)[ty==1],vasym_err[ty==1],label="HII")
-    ax.scatter((n_nans/n_not_nans)[ty!=1],vasym_err[ty!=1],ec="magenta",color='tab:blue',label="AGN")
+    ax.scatter((n_nans/n_not_nans),vasym_err,label="HII")
+    ax.scatter((n_nans/n_not_nans),vasym_err,ec="magenta",color='tab:blue',label="AGN")
     ax.hlines(0.4,xmin=-1,xmax=2,ls="dashed",color="k")
     ax.vlines(0.3,ymin=0,ymax=0.4,ls="dashed",color="k")
     ax.set_ylabel(r"$\sigma (v_{\rm asym})$")
@@ -351,6 +351,6 @@ def vasyms_nans():
                        "NaNs_at_ellipse":n_nans/n_not_nans})
     df.to_csv("MAGPI_csv/MAGPI_ellipse_nans.csv",index=False)
 
-# vasyms_nans()
-maps_check()
+vasyms_nans()
+#maps_check()
 
